5f3a20eaeba647f7571960af683c4a504219e561
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-11-08 18:01:10 +0900

    메뉴 디자인

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -7,14 +7,32 @@ export default function Menu() {
   return (
     <Accordion isCompact>
       <AccordionItem key="1" aria-label="메뉴1" title="메뉴1">
-        <Link href="/pages/dashboard">dashboard</Link><br />
-        <Link href="/pages/indivisual">indivisual</Link>
+        <Link href="/pages/dashboard" className="text-blue-500 hover:underline">
+          dashboard
+        </Link>
+        <br />
+        <Link
+          href="/pages/indivisual"
+          className="text-blue-500 hover:underline"
+        >
+          indivisual
+        </Link>
       </AccordionItem>
       <AccordionItem key="2" aria-label="메뉴2" title="메뉴2">
-        <Link href="/pages/indivisual">indivisual</Link>
+        <Link
+          href="/pages/indivisual"
+          className="text-blue-500 hover:underline"
+        >
+          indivisual
+        </Link>
       </AccordionItem>
       <AccordionItem key="3" aria-label="메뉴3" title="메뉴3">
-        <Link href="/pages/teamleader">teamleader</Link>
+        <Link
+          href="/pages/teamleader"
+          className="text-blue-500 hover:underline"
+        >
+          teamleader
+        </Link>
       </AccordionItem>
     </Accordion>
   );
